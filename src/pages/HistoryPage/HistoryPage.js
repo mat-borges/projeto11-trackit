2555e5f558f3028276c450ea8a662b0f761a0d3c
Mateus Borges <borgesm.work@gmail.com>
2022-10-18 17:59:00 -0300

    static layout done

--- src/pages/HistoryPage/HistoryPage.js
+++ src/pages/HistoryPage/HistoryPage.js
@@ -1,7 +1,37 @@
+import { baseColor, textColor } from '../../constants/colors';
+
+import styled from 'styled-components';
+
 export default function HistoryPage() {
 	return (
-		<div>
-			<p> History </p>
-		</div>
+		<HistoryContainer>
+			<HistoryHeader>
+				<h1>Histórico</h1>
+				<h2>Em breve você poderá ver o histórico dos seus hábitos aqui!</h2>
+			</HistoryHeader>
+		</HistoryContainer>
 	);
 }
+
+const HistoryContainer = styled.div`
+	margin: 70px 18px;
+`;
+
+const HistoryHeader = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin-bottom: 25px;
+	padding-top: 23px;
+	h1 {
+		color: ${baseColor};
+		font-size: 23px;
+		line-height: 29px;
+		margin-bottom: 20px;
+	}
+	h2 {
+		color: ${textColor};
+		font-size: 18px;
+		line-height: 22px;
+	}
+`;
--- src/pages/TodayPage/TodayPage.js
+++ src/pages/TodayPage/TodayPage.js
@@ -1,15 +1,82 @@
+import { accentColor, baseColor, textColor } from '../../constants/colors';
+
+import checkIcon from '../../assets/images/checkmark.svg';
 import styled from 'styled-components';
 
 export default function TodayPage() {
 	return (
 		<TodayContainer>
-			<p> Today </p>
+			<TodayHeader>
+				<h1>Terça, 18/10</h1>
+				<h2>Nenhum hábito concluído ainda</h2>
+			</TodayHeader>
+			<DayHabits>
+				<div>
+					<h1>Ler 1 capítulo de livro</h1>
+					<p>Sequência atual: 3 dias</p>
+					<p>Seu recorde: 5 dias</p>
+				</div>
+				<button>
+					<img src={checkIcon} alt="check icon" title="concluir hábito" />
+				</button>
+			</DayHabits>
 		</TodayContainer>
 	);
 }
 
 const TodayContainer = styled.div`
-	width: 100%;
-	height: 100%;
-	background-color: #e5e5e5;
+	margin: 70px 18px;
+`;
+
+const TodayHeader = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin-bottom: 25px;
+	padding-top: 23px;
+	h1 {
+		color: ${baseColor};
+		font-size: 23px;
+		line-height: 23px;
+		margin-bottom: 5px;
+	}
+	h2 {
+		color: #bababa;
+		font-size: 18px;
+		line-height: 23px;
+	}
+`;
+
+const DayHabits = styled.div`
+	box-sizing: border-box;
+	min-height: 94px;
+	height: fit-content;
+	background-color: #ffffff;
+	border-radius: 5px;
+	display: flex;
+	justify-content: space-between;
+	padding: 15px;
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		color: ${textColor};
+		h1 {
+			font-size: 20px;
+			line-height: 25px;
+			margin-bottom: 7px;
+		}
+		h2 {
+			font-size: 13px;
+			line-height: 16px;
+		}
+	}
+	button {
+		background-color: #e7e7e7;
+		img {
+			width: 69px;
+			height: 69px;
+			filter: invert(100%) sepia(4%) saturate(0%) hue-rotate(273deg) brightness(105%) contrast(106%);
+		}
+	}
 `;
